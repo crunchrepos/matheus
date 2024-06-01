601014b6ab2dcf060ee0a794f966ea8e75f94e38
crunchrepos <crunch.repos@crunch.is>
2024-06-01 03:37:02 -0300

    added ecs_task security_group on ingress rule at rds_sg

--- .infra/terraform/security_groups.tf
+++ .infra/terraform/security_groups.tf
@@ -9,6 +9,12 @@ resource "aws_security_group" "rds_sg" {
     protocol    = "tcp"
     cidr_blocks = var.vpc_private_subnets
   }
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    security_groups = aws_security_group.ecs_tasks.id
+  }
 
   egress {
     from_port   = 0
